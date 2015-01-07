f1f31fd60cade29baccaf62c2a4e5f921654410e
Denis Laliberté <denis@tp1.ca>
2015-01-07 10:40:54 -0500

    add count one item

--- spec/StackSpec.js
+++ spec/StackSpec.js
@@ -4,4 +4,10 @@ describe("Stack", function() {
     expect(stack.size()).toEqual(0);
     }
   );
+  it("Stack with one item size should return 1", function() {
+    stack = new Stack();
+    stack.add(3);
+    expect(stack.size()).toEqual(1);
+    }
+  );
 });
--- src/Stack.js
+++ src/Stack.js
@@ -1,1 +1,10 @@
 
+function Stack() {
+  this.count = 0;
+  this.add = function() {
+    this.count =1
+  }
+  this.size = function() {
+    return this.count;
+  }
+}
