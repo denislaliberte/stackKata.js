b0c7bd412ac30f741ddde715b51417b4a5a689cc
Denis Laliberté <denis@tp1.ca>
2015-01-07 15:52:11 -0500

    rename item to items

--- src/Stack.js
+++ src/Stack.js
@@ -1,14 +1,14 @@
 function ImmutableStack(items,count) {
 
   var count = (typeof(count)==='undefined') ? 0 : count;
-  var item =(typeof(items)==='undefined') ? []: items;
+  var items =(typeof(items)==='undefined') ? []: items;
 
   this.count = function() {
     return count;
   }
 
   this.push = function(newItem) {
-    newItems = item;
+    newItems = items;
     newItems[count + 1] = newItem;
     return new ImmutableStack(newItems, count + 1);
   }
@@ -17,14 +17,13 @@ function ImmutableStack(items,count) {
     if(count ==0) {
       throw new Error("Empty stack");
     }
-    return new ImmutableStack(item, count - 1);
+    return new ImmutableStack(items, count - 1);
   }
 
   this.peek = function() {
     if(count ==0) {
       throw new Error("Empty stack");
     }
-    return item[count];
+    return items[count];
   }
-
 }
